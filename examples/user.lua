0fc6cb60381e797393a63c144baa1739fa3351e0
Abhishek Dwivedi <abhishekkumardwivedi@gmail.com>
2016-07-15 00:37:21 +0530

    added gpio interrup interface

--- examples/user.lua
+++ examples/user.lua
@@ -3,21 +3,47 @@ dev_id      = "table1"      -- this is the id given to this mqtt client
                             -- and has to be unique for each.
 wifi_ssid   = "BOX2"
 wifi_passwd = "test12345"
-mqtt_host   = 192.168.0.103 --mqtt broker server ip address
+mqtt_host   = "192.168.0.104" --mqtt broker server ip address
+mqtt_port    = 8884
 --------------------------------------------------------------------------/
 
 -----------[ DONT change these. Keep in sync with server ]----------------
-mqtt_port    = 8884
 topic_order  = "order"
 topic_ticket = "ticket"
 topic_cancel = "cancel"
 --------------------------------------------------------------------------/
 
+-------------------------
+--  IO      ESP8266 pin
+--  0[*]    GPIO16  --  [pin 0 doesn't support interrupt]
+
+--  1       GPIO5   
+--  2       GPIO4   
+--  3       GPIO0   
+--  4       GPIO2   
+--  5       GPIO14  
+--  6       GPIO12  
+--  7       GPIO13
+--  8       GPIO15
+
+--  9       GPIO3
+--  10      GPIO1
+--  11      GPIO9
+--  12      GPIO10
+-----------------------
+-----[input pins to be used, config it according to h/w connection] ---------
+pinTicket   = 5
+pinOrder    = 6
+pinCancel   = 7
+-----------------------------------------------------------------------------/
+
+----------------- All config ends here --------------------------------------/
+
 print("WiFi connection initiated.....")
 print(wifi.sta.getip())
 --nil
 wifi.setmode(wifi.STATION)
-wifi.sta.config("BOX2","test12345")
+wifi.sta.config(wifi_ssid, wifi_passwd)
 wifi.sta.connect()
 tmr.alarm(1, 1000, 1, function()
      if wifi.sta.getip() == nil then
@@ -35,7 +61,7 @@ tmr.alarm(1, 1000, 1, function()
         end
 
         -- Lua: mqtt.Client(clientid, keepalive, user, pass)
-        m=mqtt.Client("nodemcu1",60,"test","test123")
+        m=mqtt.Client(dev_id, 60, "test", "test123")
 
         m:on("connect",function(m)
             print("connection "..node.heap())
@@ -49,7 +75,7 @@ tmr.alarm(1, 1000, 1, function()
         m:on("message", printMsg )
 
         -- Lua: mqtt:connect( host, port, secure, auto_reconnect, function(client) )
-        m:connect("192.168.0.103",8884,0,1)
+        m:connect(mqtt_host, mqtt_port, 0, 1)
         tmr.alarm(0,10000,1,function()
             m:publish("/breathing/"..dev_id, dev_id.." breathing "..tmr.time(),0,0) -- keep server know that
                                                              -- I am alive. For Debugging.
@@ -57,3 +83,41 @@ tmr.alarm(1, 1000, 1, function()
          -------------------------------------------
      end
 end)
+
+oldlevel_ticket = 0
+function ticketISR(level)
+  print("ticket ISR");
+  if level == 1 and m~=nil and oldlevel_ticket == 0 then
+     oldlevel_ticket = 1
+     -- rising edge
+     m:publish("/request/"..dev_id, topic_ticket, 0, 0)
+  else
+     oldlevel_ticket = 0
+  end
+end
+
+function orderISR(level)
+  print("order ISR");
+  if level == 1 and m~=nil then
+     -- rising edge
+     m:publish("/request/"..dev_id, topic_order, 0, 0)
+  else
+  end
+end
+
+function cancelISR(level)
+  print("cancel ISR");
+  if level == 1 and m~=nil then
+     -- rising edge
+     m:publish("/request/"..dev_id, topic_cancel, 0, 0)
+  else
+  end
+end
+
+gpio.mode(pinTicket, gpio.INPUT);
+gpio.mode(pinOrder, gpio.INT);
+gpio.mode(pinCancel, gpio.INT);
+
+gpio.trig(pinTicket, "high", ticketISR)
+gpio.trig(pinOrder, "high", orderISR)
+gpio.trig(pinCancel, "high", cancelISR)
